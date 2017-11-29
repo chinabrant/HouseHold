0cca78f6d53f060992b556dbefc2e7a70a7d129c
brant <sjwu1234@gmail.com>
2017-11-29 17:43:48 +0800

    update

---
+++ HouseHold/Classes/Strategy/View/IntroduceView.swift
@@ -0,0 +1,24 @@
+//
+//  IntroduceView.swift
+//  HouseHold
+//
+//  Created by brant on 2017/11/29.
+//  Copyright © 2017年 brant. All rights reserved.
+//
+
+import UIKit
+
+
+/// 列表显示的一句话描述
+class IntroduceView: UIView {
+
+    
+    
+    // MARK: lazy load
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+
+}
